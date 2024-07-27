9a32ffc165833090fe891eed464db9e1112b4959
Moses Karani <karanim594@gmail.com>
2024-07-27 22:28:06 +0300

    included necesarry routes

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function GET() {
   try {
   } catch (error) {
     console.error(error);
---
+++ app/api/workertasks/route.js
@@ -0,0 +1,6 @@
+export async function GET(req) {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
